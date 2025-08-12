be44741060996896363680bb41e6e66d9a200a70
Ludwing Chiri <ludwing.chiri@hotmail.com>
2025-08-12 14:12:11 -0500

    T172 api and services added

---
+++ src/main/java/com/sudamericano/bank/domain/ports/inputs/catalog/T172UseCase.java
@@ -0,0 +1,9 @@
+package com.sudamericano.bank.domain.ports.inputs.catalog;
+
+import com.sudamericano.bank.domain.model.catalog.T172Dto;
+
+import java.util.List;
+
+public interface T172UseCase {
+    List<T172Dto> findAll();
+}
---
+++ src/main/java/com/sudamericano/bank/domain/ports/outputs/catalog/T172RepositoryPort.java
@@ -0,0 +1,9 @@
+package com.sudamericano.bank.domain.ports.outputs.catalog;
+
+import com.sudamericano.bank.domain.model.catalog.T172Dto;
+
+import java.util.List;
+
+public interface T172RepositoryPort {
+    List<T172Dto> findAll();
+}
