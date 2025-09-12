551d7d864e5b4f2109855fd892ef4db4eb35793d
Ludwing Chiri <ludwing.chiri@hotmail.com>
2025-09-12 09:46:01 -0500

    Structure R02 - NUevos campos

---
+++ src/main/java/com/sudamericano/bank/infrastructure/mapper/catalog/T36_6Mapper.java
@@ -0,0 +1,14 @@
+package com.sudamericano.bank.infrastructure.mapper.catalog;
+
+import com.sudamericano.bank.domain.model.catalog.T36_6Dto;
+import com.sudamericano.bank.infrastructure.persistence.entity.catalog.T36_6Entity;
+
+public class T36_6Mapper {
+    public T36_6Dto toDto(T36_6Entity entity){
+        return new T36_6Dto(
+                entity.getId(),
+                entity.getCodigo(),
+                entity.getDescripcion()
+        );
+    }
+}
