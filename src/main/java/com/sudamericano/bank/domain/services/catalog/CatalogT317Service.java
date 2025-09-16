5fbc507b32615cc707467b051d3fa6266206ec4f
darknicht82 <christian.david.aguirre@gmail.com>
2025-09-16 15:53:06 -0500

    Cambios T317

--- src/main/java/com/sudamericano/bank/domain/services/catalog/CatalogT317Service.java
+++ src/main/java/com/sudamericano/bank/domain/services/catalog/CatalogT317Service.java
@@ -3,18 +3,21 @@ package com.sudamericano.bank.domain.services.catalog;
 import com.sudamericano.bank.domain.model.catalog.CatalogT317;
 import com.sudamericano.bank.domain.ports.inputs.catalog.CatalogT317UseCase;
 import com.sudamericano.bank.domain.ports.outputs.catalog.CatalogT317RepositoryPort;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class CatalogT317Service implements CatalogT317UseCase {
+
     private final CatalogT317RepositoryPort catalogT317RepositoryPort;
+
+    public CatalogT317Service(CatalogT317RepositoryPort catalogT317RepositoryPort) {
+        this.catalogT317RepositoryPort = catalogT317RepositoryPort;
+    }
 
     @Override
     public List<CatalogT317> getAllCatalogT317() {
-        return catalogT317RepositoryPort.getAllCatalogT317();
+        return catalogT317RepositoryPort.findAll();
     }
 }
