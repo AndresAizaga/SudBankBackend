672e0b36d775c462811f28f1f2fb7418085aab7b
DylanGReinosoT <dgreinoso@espe.edu.ec>
2025-09-18 15:32:18 -0500

    Structure R05 - Cambios

--- src/main/java/com/sudamericano/bank/infrastructure/controller/structure/R/R05Controller.java
+++ src/main/java/com/sudamericano/bank/infrastructure/controller/structure/R/R05Controller.java
@@ -50,15 +50,12 @@ public class R05Controller {
                             new ResponseDTO(catalogT4.getId(), catalogT4.getDescripcion())
                     ));
 
-            resume.setIdentificacionSujeto(dto.getIdentificacionSujeto());
-            resume.setNumeroOperacion(dto.getNumeroOperacion());
 
             T208Dto transaccion = t208UseCase.findById(dto.getCodigoFormaCancelacion());
             if(transaccion != null){
                 resume.setCodigoTipoTransaccion(new ResponseDTO(transaccion.getId(), transaccion.getDescripcion()));
             }
 
-            resume.setFechaCancelacion(dto.getFechaCancelacion());
 
             T39Dto cancelacion = t39UseCase.findById(dto.getCodigoFormaCancelacion());
             if(transaccion != null){
@@ -70,6 +67,10 @@ public class R05Controller {
                     .ifPresent(catalogT29 -> resume.setCodigoCalificacion(
                             new ResponseDTO(catalogT29.getId(), catalogT29.getDescripcion())
                     ));
+
+            resume.setIdentificacionSujeto(dto.getIdentificacionSujeto());
+            resume.setNumeroOperacion(dto.getNumeroOperacion());
+            resume.setFechaCancelacion(dto.getFechaCancelacion());
 
         }
         return resumes;
